2096a18922351000b01108d98978a37501b1b168
JoelVLR <jvlopezrojas@gmail.com>
2023-01-16 10:10:23 -0300

    sesion funciones

--- funciones/src/ejercicio_sesion2_funciones.java
+++ funciones/src/ejercicio_sesion2_funciones.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class ejercicio_sesion2_funciones {
     public static void main(String[] args) {
-    double precioTotal = obtenerPrecio(1000);
-    System.out.println(precioTotal);
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Introduce un precio");
+        int precioBruto=teclado.nextInt();
+        double precioTotal = obtenerPrecio(precioBruto);
+        System.out.println("Precio con IVA incluido " + " " + precioTotal);
     }
     /**En Chile el IVA equivale a un 19% agregado al precio final del producto
     los precios son numeros enteros sin decimales así que use el Int pero para recibirlo tuve que usar el double
